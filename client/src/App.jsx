2a4e581f5f6f8f2d4676481817a09b3b44e8e634
Nevermeant <timothy.jake.holmes2@gmail.com>
2024-10-01 17:27:42 -0700

    Setting up react

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Route, Routes, Outlet, Navigate, Link, useNavigate, useLocation } from "react-router-dom";
+
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
