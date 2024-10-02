bbabfcc04993b5856ef83989bd0a6d750da08dcc
Nevermeant <timothy.jake.holmes2@gmail.com>
2024-10-01 17:50:13 -0700

    Setting up routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Route, Routes, Outlet, Navigate, Link, useNavigate, useLocation } from "react-router-dom";
+import { Login } from "./pages/Login.jsx";
+import { Dashboard } from "./pages/Dashboard.jsx";
+import { WorkOrder } from "./pages/WorkOrder.jsx";
 
 
 function App() {
 
   return (
     <>
+
+      <Routes>
+        <Route path="/" element={ <Login />} />
+        <Route path="/dashboard" element={ <Dashboard />} />
+        <Route path="/workorder" element={ <WorkOrder />} />
+      </Routes>
       
     </>
   )
